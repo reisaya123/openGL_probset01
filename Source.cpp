39e313ddac1ae25033975f0cebccf897afddf8fd
Siroy <siroy.ayesha@gmail.com>
2022-02-08 20:18:40 +0800

    feat: create two triangles with two different vao and vbo

--- Source.cpp
+++ Source.cpp
@@ -95,30 +95,53 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    float vertices[] = {
-        // x      y       z
-
+    float vertices1[] = {
          -0.5f, -0.5f, 0.0f,
          0.0f, -0.5f, 0.0f,
          -0.25f, 0.25f, 0.0f,
+    };
 
+    float vertices2[] = {
          0.0f, -0.50f, 0.0f,
          0.5f, -0.5f, 0.0f,
          0.25f, 0.25f, 0.0f
-
     };
 
-    unsigned int vbo, vao;
-    glGenBuffers(1, &vbo);
-    glGenVertexArrays(1, &vao);
+    //float vertices[] = {
+    //    // x      y       z
+
+    //     -0.5f, -0.5f, 0.0f,
+    //     0.0f, -0.5f, 0.0f,
+    //     -0.25f, 0.25f, 0.0f,
+
+    //     0.0f, -0.50f, 0.0f,
+    //     0.5f, -0.5f, 0.0f,
+    //     0.25f, 0.25f, 0.0f
+
+    //};
+
+    unsigned int vbo1, vbo2, vao1, vao2;
+    glGenBuffers(1, &vbo1);
+    glGenBuffers(1, &vbo2);
+    glGenVertexArrays(1, &vao1);
+    glGenVertexArrays(1, &vao2);
 
 
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindVertexArray(vao1);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    glBindVertexArray(vao2);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -130,9 +153,13 @@ int main()
 
         glUseProgram(shaderProgram);
 
-        //render
-        glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Triangle 1
+        glBindVertexArray(vao1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Triangle 2
+        glBindVertexArray(vao2);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
